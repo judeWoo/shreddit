3362f7d60ed6712273065c2d13d6c9595d4d274f
Jude Hokyoon Woo <kw021992@gmail.com>
2021-05-26 23:57:02 +0900

    update

--- server/deploy.sh
+++ server/deploy.sh
@@ -3,6 +3,6 @@
 echo What should the version be?
 read VERSION
 
-docker build -t benawad/lireddit:$VERSION .
-docker push benawad/lireddit:$VERSION
-ssh root@64.227.13.208 "docker pull benawad/lireddit:$VERSION && docker tag benawad/lireddit:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
+docker build -t judeWoo/shreddit:$VERSION .
+docker push judeWoo/shreddit:$VERSION
+ssh root@64.227.13.208 "docker pull judeWoo/shreddit:$VERSION && docker tag judeWoo/shreddit:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -61,7 +61,7 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
       <Flex flex={1} m="auto" align="center" maxW={800}>
         <NextLink href="/">
           <Link>
-            <Heading>LiReddit</Heading>
+            <Heading>shreddit</Heading>
           </Link>
         </NextLink>
         <Box ml={"auto"}>{body}</Box>
